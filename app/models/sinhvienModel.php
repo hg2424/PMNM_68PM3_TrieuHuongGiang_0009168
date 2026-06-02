1d33cd72d00c7968ebcf55c04a05bc18e6b91488
hg2424 <123trieugiang@gmail.com>
2026-06-02 21:38:39 +0700

    feat<sinhvien>: create Sinhvien

--- app/models/sinhvienModel.php
+++ app/models/sinhvienModel.php
@@ -13,6 +13,17 @@
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
+        public function create($data) {
+            $query = "INSERT INTO sinhvien (ma_sv, ho_ten, gioi_tinh, ngay_sinh, dia_chi, lop) VALUES (:ma_sv, :ho_ten, :gioi_tinh, :ngay_sinh, :dia_chi, :lop)";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':ma_sv', $data['ma_sv']);
+            $stmt->bindParam(':ho_ten', $data['ho_ten']);
+            $stmt->bindParam(':gioi_tinh', $data['gioi_tinh']);
+            $stmt->bindParam(':ngay_sinh', $data['ngay_sinh']);
+            $stmt->bindParam(':dia_chi', $data['dia_chi']);
+            $stmt->bindParam(':lop', $data['lop']);
+            return $stmt->execute();
+        }
         
     }
 
